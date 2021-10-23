aefbbfbf07b8c6e6b6d09b4969e50acea4dcd757
Corey Lynch <91030401+coreylynch00@users.noreply.github.com>
2021-10-23 23:28:22 +0100

    Delete Python/FirstYear/Labs/6 directory

--- Python/FirstYear/Labs/6/bored.py
+++
@@ -1,5 +0,0 @@
-# Corey Lynch 23/10/2019
-
-answer = "no"
-while answer == "no":
-    answer = input("Are you bored yet?")
--- Python/FirstYear/Labs/6/numbers2.py
+++
@@ -1,9 +0,0 @@
-# Corey Lynch 23/10/2019
-
-x = 1
-y = int(input("Enter a number."))
-
-while x <= y:
-    print(x, end=" ")
-    x += 1
-print()
