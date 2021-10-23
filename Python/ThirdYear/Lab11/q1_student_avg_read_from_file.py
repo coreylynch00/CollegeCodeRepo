a986bc738470d6e3003f8e7a43969f1aa07204a9
coreylynch00 <coreylynch4@gmail.com>
2021-10-23 21:44:34 +0100

    Lab11/Q1A made funtion

--- Python/ThirdYear/Lab11/q1_student_avg_read_from_file.py
+++ Python/ThirdYear/Lab11/q1_student_avg_read_from_file.py
@@ -2,20 +2,23 @@
 
 # Read data from file and calculate average grade
 
-# Open file
-file = open("studentData", "r")
+def read_average():
+    # Open file
+    file = open("studentData", "r")
 
-# Iterate through each line
-for line in file:
-    # Split each item into separate strings - (name and 3 grades)
-    data = line.split()
-    # Get student name - index 0 = name
-    name = data[0]
-    # Calculate average - index 1 = grade 1, index 2 = grade 2, etc
-    avg = (int(data[1])+int(data[2])+int(data[3]))/3
-    # Print data
-    print(f"{name}, {avg:.2f}")
+    # Iterate through each line
+    for line in file:
+        # Split each item into separate strings - (name and 3 grades)
+        data = line.split()
+        # Get student name - index 0 = name
+        name = data[0]
+        # Calculate average - index 1 = grade 1, index 2 = grade 2, etc
+        avg = (int(data[1])+int(data[2])+int(data[3]))/3
+        # Print data
+        print(f"{name}, {avg:.2f}")
 
-# Close file
-file.close()
+    # Close file
+    file.close()
 
+
+read_average()
